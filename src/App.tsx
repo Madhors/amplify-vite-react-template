51882ffa5934eba3bf326bc6781906f7617d2ee6
EC2 Default User <ec2-user@ip-172-31-42-46.ap-northeast-1.compute.internal>
2024-09-17 01:21:38 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import outputs from "../amplify_outputs.json";
@@ -6,14 +6,39 @@ import outputs from "../amplify_outputs.json";
 Amplify.configure(outputs);
 
 export default function App() {
+  const { authStatus } = useAuthenticator(context => [context.authStatus]);
+
   return (
-<Authenticator signUpAttributes={['email', 'phone_number']}>
-  {({ signOut }) => (
-    <main>
-      <h1>Hello</h1>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-  )}
-</Authenticator>
+    <>
+      {authStatus === 'configuring' && 'Loading...'}
+      {authStatus !== 'authenticated' ? (
+        <LoginScreen />
+      ) : (
+        <Home />
+      )}
+    </>
   );
 }
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <div>Authentication required</div>
+      <Authenticator>
+        {({ signIn }) => <button onClick={signIn}>Login</button>}
+      </Authenticator>
+    </div>
+  );
+};
+
+const Home = () => {
+  const { signOut } = useAuthenticator(context => [context.user, context.signOut]);
+
+  return (
+    <div>
+      <h1>Welcome to the Home page!</h1>
+      {/* Add your home page content here */}
+      <button onClick={signOut}>Sign Out</button>
+    </div>
+  );
+};
